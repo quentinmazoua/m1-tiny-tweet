f371b16820578da3f11a9010a105b0b407176219
Corentin Marionneau <corentin.marionneau@gmail.com>
2017-12-21 21:00:25 +0100

    Added a response text instead of void return for API methods

---
+++ src/main/java/api/ResponseAPI.java
@@ -0,0 +1,20 @@
+package api;
+
+public class ResponseAPI {
+
+	private String content;
+
+	public ResponseAPI(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
